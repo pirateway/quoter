f022128d33c2906d3999edbb02192fbadb6585f5
pirateway <mosro44an@yandex.ru>
2019-09-09 22:34:53 +0300

    Create own property file base configuration by PropertyFileApplicationContext("context.properties")

--- src/main/java/com/asus/quoter/App.java
+++ src/main/java/com/asus/quoter/App.java
@@ -1,8 +1,13 @@
 package com.asus.quoter;
 
+import com.asus.quoter.api.service.Quoter;
 import com.asus.quoter.config.JavaConfig;
+import com.asus.quoter.config.PropertyFileApplicationContext;
+import org.springframework.context.annotation.AnnotatedBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ConfigurationClassPostProcessor;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -15,6 +20,9 @@ public class App
     {
         //ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext("com.asus.quoter");
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(JavaConfig.class);
+        PropertyFileApplicationContext context = new PropertyFileApplicationContext("context.properties");
+        context.getBean(Quoter.class).sayQuote();
+
     }
 }
